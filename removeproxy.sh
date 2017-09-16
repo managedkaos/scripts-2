03d2f3ec815324c16639f2fe7abd73d460009138
Harsh Zalavadiya <zhnebula@gmail.com>
2017-09-17 02:06:36 +0530

    Create removeproxy.sh

---
+++ removeproxy.sh
@@ -0,0 +1,9 @@
+git config --global --unset https.proxy
+git config --global --unset http.proxy
+export http_proxy=
+export https_proxy=
+export HTTP_PROXY=
+export HTTPS_PROXY=
+export PROXY=
+npm config rm proxy
+npm config rm https-proxy
