5a25d0f0e6f422350f04f1ede110c8a9132bfe93
Harsh Zalavadiya <zhnebula@gmail.com>
2017-09-17 02:05:55 +0530

    Create setupproxy.sh

---
+++ setupproxy.sh
@@ -0,0 +1,9 @@
+git config --global http.proxy http://user:pass@host:port
+npm config set proxy http://user:pass@host:port
+npm config set https-proxy http://user:pass@host:port
+export HTTP_PROXY=http://user:pass@host:port
+export HTTPS_PROXY=http://user:pass@host:port
+export http_proxy=http://user:pass@host:port
+export https_proxy=http://user:pass@host:port
+export ALL_PROXY=http://user:pass@host:port
+export PROXY=http://user:pass@host:port
