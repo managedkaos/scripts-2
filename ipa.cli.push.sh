291ddbd1dabbf6c71d6f9f1f8697739766355671
Harsh Zalavadiya <zhnebula@gmail.com>
2018-05-17 11:45:18 +0530

    Update ipa.cli.push.sh

--- ipa.cli.push.sh
+++ ipa.cli.push.sh
@@ -1,5 +1,10 @@
 # Install ipa through cli
-brew install libimobiledevice
-brew install ideviceinstaller
+brew uninstall ideviceinstaller
+brew uninstall libimobiledevice
+brew install --HEAD libimobiledevice
+brew link --overwrite libimobiledevice
+brew install --HEAD  ideviceinstaller
+brew link --overwrite ideviceinstaller
+sudo chmod -R 777 /var/db/lockdown/
 # Push to device
 ideviceinstaller -i <Path_to_your_ipa> 
